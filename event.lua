f34da3ba2661640d62e3b8e6e09dfaec815ee28d
J.C. Moyer <jmoyer1992@gmail.com>
2015-08-13 06:44:23 -0400

    Rename event methods for clarity and implement event emitters

--- event.lua
+++ event.lua
@@ -37,14 +37,14 @@ function event.new()
   return setmetatable(instance, event)
 end
 
--- Connects `f` to this event. When this event is raised, `f` will be called
--- with the same parameters passed to `raise`.
-function event:connect(f)
+-- Adds `f` to the list of listeners for this event. When this event is raised,
+-- `f` will be called with the same parameters passed to `raise`.
+function event:add(f)
   table.insert(self.callbacks, f)
 end
 
--- Disconnects `f` from this event.
-function event:disconnect(f)
+-- Removes `f` from this event.
+function event:remove(f)
   for i = 1, #self.callbacks do
     if self.callbacks[i] == f then
       return table.remove(self.callbacks, i)
@@ -52,24 +52,129 @@ function event:disconnect(f)
   end
 end
 
--- Disconnects all functions from this event.
+-- Removes all listeners from this event.
 function event:clear()
   for i = #self.callbacks, 1, -1 do
     table.remove(self.callbacks, i)
   end
 end
 
--- Returns the number of functions connected to this event.
+-- Returns the number of listeners associated with this event.
 function event:len()
   return #self.callbacks
 end
 
--- Raises this event. This calls all functions that have been connected to this
--- event.
+-- Raises this event. All listeners connected to this event will be called with
+-- the same parameters passed to `event:raise()`.
 function event:raise(...)
   for i = 1, #self.callbacks do
     self.callbacks[i](...)
   end
 end
+
+--
+-- Emitter implementation
+--
+local emitter = module.new()
+
+local eventproxy = {
+  -- add nonexistent events to the table
+  __index = function(t, k)
+    local e = event.new()
+    rawset(t, k, e)
+    return e
+  end
+}
+
+local emitterfns = {'emit', 'on', 'remove', 'clear'}
+
+function emitter.new()
+  local instance = {
+    events = setmetatable({}, eventproxy)
+  }
+  return setmetatable(instance, emitter)
+end
+
+-- Inserts an emitter object into the table `t` named `_emitter`. When
+-- instantiating an object, use this function to compose emitter functionality.
+-- Returns `t` for convenience.
+function emitter.compose(t)
+  t._emitter = emitter.new()
+  return t
+end
+
+-- Declares `t` to have methods similar to `emitter`. The following fields will
+-- be assigned:
+--
+-- * `t.emit`
+-- * `t.on`
+-- * `t.remove`
+-- * `t.clear`
+--
+-- Use this in combination with `emitter.compose` to add emitter functionality
+-- to your own types:
+--
+-- ```
+-- local timer = module.new()
+--
+-- event.emitter.declare(timer)
+-- -- at this point, timer.emit, timer.on, timer.remove, and timer.clear are
+-- -- callable functions
+--
+-- function timer.new()
+--   return event.emitter.compose(setmetatable({tickcount = 0}, timer))
+-- end
+--
+-- function timer:tick()
+--   self.tickcount = self.tickcount + 1
+--   self:emit('tick', self)
+-- end
+--
+-- local t = timer.new()
+-- t:on('tick', function(timer)
+--   print('timer has ticked ' .. timer.tickcount .. ' time(s)')
+-- end)
+-- t:tick() -- 'timer has ticked 1 time(s)'
+-- t:tick() -- 'timer has ticked 2 time(s)'
+-- ```
+function emitter.declare(t)
+  -- declare wrapper functions on `t`
+  for i = 1,#emitterfns do
+    local name = emitterfns[i]
+    -- since we can't use colon syntax here, we need to declare self
+    t[name] = function(self, ...)
+      -- here, `self` is an instance of `t`; similar to above, we need to take
+      -- its `_emitter` field and call a method on it, passing `emitter` as the
+      -- first parameter so that `self` is correct when control is passed to
+      -- one of the emitter functions
+      local emitter = self._emitter
+      return emitter[name](emitter, ...)
+    end
+  end
+end
+
+-- Emits a `name` event. All listeners for `name` will be called with any
+-- additional parameters.
+function emitter:emit(name, ...)
+  self.events[name]:raise(...)
+end
+
+-- Adds `f` as a listener for `name` events.
+function emitter:on(name, f)
+  self.events[name]:add(f)
+end
+
+-- Removes `f` from the list of listeners `name` events.
+function emitter:remove(name, f)
+  self.events[name]:remove(f)
+end
+
+-- Clears all listeners for `name` events.
+function emitter:clear(name)
+  self.events[name]:clear()
+end
+
+-- expose `emitter` through the `event` module
+event.emitter = emitter
 
 return event
--- tests/test-event.lua
+++ tests/test-event.lua
@@ -1,25 +1,26 @@
 require('path')
 local framework = require('tt')
 local event = require('hug.event')
+local module = require('hug.module')
 
-local function connect()
+local function add()
   local e = event.new()
   local n = false
-  e:connect(function()
+  e:add(function()
     n = true
   end)
   e:raise()
   framework.compare(true, n)
 end
 
-local function disconnect()
+local function remove()
   local e = event.new()
   local n = false
   local function f()
     n = true
   end
-  e:connect(f)
-  e:disconnect(f)
+  e:add(f)
+  e:remove(f)
   e:raise()
   framework.compare(false, n)
 end
@@ -30,7 +31,7 @@ local function raise()
   local function f(x)
     n = n + x
   end
-  e:connect(f)
+  e:add(f)
   e:raise(1)
   framework.compare(1, n)
   e(1)
@@ -39,9 +40,9 @@ end
 
 local function len()
   local e = event.new()
-  e:connect(function() end)
+  e:add(function() end)
   framework.compare(1, e:len())
-  e:connect(function() end)
+  e:add(function() end)
   framework.compare(2, e:len())
   -- __len not supported on tables in 5.1 and, by extension, luajit with
   -- default compilation options
@@ -50,9 +51,55 @@ local function len()
   end
 end
 
+local function emitter()
+  local e = event.emitter.new()
+  local n = 0
+  local function increment()
+    n = n + 1
+  end
+  local function decrement()
+    n = n - 1
+  end
+  e:on('increment', increment)
+  e:on('decrement', decrement)
+  e:emit('increment')
+  framework.compare(1, n)
+  e:emit('decrement')
+  framework.compare(0, n)
+  e:remove('increment', increment)
+  e:emit('increment')
+  framework.compare(0, n)
+  e:clear('decrement')
+  e:emit('decrement')
+  framework.compare(0, n)
+end
+
+local function emitter_composition()
+  local obj = module.new()
+  function obj.new()
+    local instance = setmetatable({}, obj)
+    event.emitter.compose(instance)
+    return instance
+  end
+  function obj:tick()
+    self:emit('tick')
+  end
+  event.emitter.declare(obj)
+
+  local a = obj.new()
+  local n = 0
+  a:on('tick', function()
+    n = n + 1
+  end)
+  a:tick()
+  framework.compare(1, n)
+end
+
 return framework.testall {
-  { 'connect', connect },
-  { 'disconnect', disconnect },
+  { 'add', add },
+  { 'remove', remove },
   { 'raise', raise },
-  { 'len', len }
+  { 'len', len },
+  { 'emitter', emitter },
+  { 'emitter composition', emitter_composition }
 }
